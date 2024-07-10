afd1fb92b8bd684b360c832eb936201c02fdc8d2
oralxan <oralxan@users.noreply.github.com>
2024-07-10 18:33:10 +0500

    j

--- src/settings/base.py
+++ src/settings/base.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-0rnqoye0-89+e9j*m(3jkrgidqsq9b@2=j0_tvv#0_+-x94^v2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
